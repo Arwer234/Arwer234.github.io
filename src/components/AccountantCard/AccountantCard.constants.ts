eac1b5272015dd0f2cd7f89266462e9d3704d776
Jakub Filipowski <filipowski.kuba@gmail.com>
2023-09-17 19:00:02 +0200

    feat: add production build

--- src/components/AccountantCard/AccountantCard.constants.ts
+++
@@ -1,6 +0,0 @@
-const GENDER_TO_CARD_SUBTITLE = {
-	male: 'Twój księgowy',
-	female: 'Twoja księgowa',
-};
-
-export { GENDER_TO_CARD_SUBTITLE };
--- src/components/AccountantCard/AccountantCardParagraph.types.ts
+++
@@ -1,8 +0,0 @@
-type AccountantCardParagraphProps = {
-	subheaderText?: string;
-	subheaderSize?: number;
-	contentType: 'email' | 'text' | 'amount' | 'header';
-	children: string;
-};
-
-export type { AccountantCardParagraphProps };
